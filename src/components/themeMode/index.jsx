8b7594163487272438d32ba174657bd0745e9fc3
nguyenlocDev <nguyenloc.tlc@gmail.com>
2024-09-01 05:34:16 +0700

    themes mui

---
+++ src/components/themeMode/index.jsx
@@ -0,0 +1,46 @@
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { Box, useColorScheme } from "@mui/material";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import SettingsBrightnessIcon from "@mui/icons-material/SettingsBrightness";
+export default function SetModeThemes() {
+  const { mode, setMode } = useColorScheme();
+  const handleChange = (event) => {
+    setMode(event.target.value);
+  };
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="dark-light-sys">{mode}</InputLabel>
+      <Select
+        labelId="dark-light-sys"
+        id="select-small"
+        value={mode}
+        label={mode}
+        onChange={handleChange}
+      >
+        <MenuItem value={"light"}>
+          <Box sx={{ display: "flex", alignItems: "center", gap: "8px" }}>
+            <LightModeIcon fontSize="small"></LightModeIcon>
+            Light
+          </Box>
+        </MenuItem>
+        <MenuItem value={"dark"}>
+          <Box sx={{ display: "flex", alignItems: "center", gap: "8px" }}>
+            <DarkModeIcon fontSize="small"></DarkModeIcon>
+            Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value={"system"}>
+          <Box sx={{ display: "flex", alignItems: "center", gap: "8px" }}>
+            <SettingsBrightnessIcon fontSize="small"></SettingsBrightnessIcon>
+            System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  );
+}
